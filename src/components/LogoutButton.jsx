df27aaf75f2730c04b065d9be850d59be3823509
Gabjea <gabjea856@gmail.com>
2024-11-19 08:52:17 +0200

    authcontext

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -1,24 +1,33 @@
-import React from "react";
-import {useNavigate} from "react-router-dom";
+// src/components/LogoutButton.js
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext"; // Import AuthContext
 
 const LogoutButton = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
 
-    const handleLogout = () => {
-        localStorage.removeItem('accessToken');
-        localStorage.removeItem('refreshToken');
-        navigate('/login');
-    };
+  if (!isAuthenticated) {
+    return null;
+  }
 
-    return(
-        <div>
-            <button onClick={handleLogout}
-                className="w-full px-4 py-2 font-qregular text-white bg-accentColour rounded-md hover:bg-linkColour focus:outline-none focus:ring-linkColour"
-            >
-                Logout
-            </button>
-        </div>
-    );
-}
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    setIsAuthenticated(false); // Update auth state
+    navigate('/login');
+  };
+
+  return (
+    <div>
+      <button
+        onClick={handleLogout}
+        className="w-full px-4 py-2 font-qregular text-white bg-accentColour rounded-md hover:bg-linkColour focus:outline-none focus:ring-linkColour"
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
 
 export default LogoutButton;
