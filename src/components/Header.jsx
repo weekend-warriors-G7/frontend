577aa4c6baef1b8967cfca565f89955555dac0e1
Gabjea <gabjea856@gmail.com>
2024-12-08 01:16:46 +0200

    redirect if not logged in

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,39 @@
-// src/components/HeaderTest.js
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import LogoutButton from "./LogoutButton";
 import AddProductButton from "./AddProductButton";
 import { AuthContext } from "../context/AuthContext"; // Import AuthContext
 import { useNavigate } from "react-router-dom";
 
-// import ProfileButton from "./ProfileButton";
-
 const Header = () => {
   const { isAuthenticated } = useContext(AuthContext); // Access auth state
   const navigate = useNavigate();
 
-  const handleNavigateToHomePage = () => {
-    navigate("/");
-  };
+  useEffect(() => {
+    // Automatically navigate based on authentication state
+    if (isAuthenticated) {
+      navigate("/");
+    } else {
+      navigate("/login");
+    }
+  }, [isAuthenticated, navigate]); // Re-run if authentication state or navigate changes
 
   return (
-    <header className="fixed top-0 left-0 w-full bg-elemColour text-white z-10">
+    <header role="banner" className="fixed top-0 left-0 w-full bg-elemColour text-white z-10">
       <div className="flex items-center justify-between p-4">
-        <h1 className="font-qbold text-xl cursor-pointer" onClick={handleNavigateToHomePage}>Handmade Shop</h1>
-        <nav className="flex items-center space-x-4">
-          {isAuthenticated && (
+        <h1 className="font-qbold text-xl cursor-pointer">Handmade Shop</h1>
+        <nav role="navigation" className="flex items-center space-x-4 md:space-x-6">
+          {isAuthenticated ? (
             <>
               <AddProductButton />
-              {/* <ProfileButton /> */}
               <LogoutButton />
             </>
+          ) : (
+            <button
+              onClick={() => navigate("/login")}
+              className="text-white hover:underline"
+            >
+              Login
+            </button>
           )}
         </nav>
       </div>
