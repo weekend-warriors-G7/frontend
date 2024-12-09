3a8910e7f129747e57f1664c84ff1beafa2a7dbb
Robert <robertfedus22@gmail.com>
2024-12-09 22:07:05 +0200

    Fixed bugs on products list

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,14 +8,17 @@ const ProductCard = ({ product }) => {
       className="bg-white p-6 rounded-lg shadow-md flex flex-col items-center hover:shadow-lg transition-shadow"
     >
       <h2 className="text-xl font-bold">{product.name}</h2>
-      <img
-        src={product.imageId}
-        alt={`${product.name}`}
-        className="h-48 w-full object-cover"
-      />
-      <p className="text-lg text-accentColour">{product.price}$</p>
+      <div className="h-48 w-full flex items-center justify-center rounded mt-2 mb-2">
+        <img
+          src={product.imageId}
+          alt={`${product.name}`}
+          className="h-full w-full object-contain"
+        />
+      </div>
+      <p className="text-lg text-accentColour font-bold">{product.price}$</p>
     </Link>
   );
 };
+
 
 export default ProductCard;
