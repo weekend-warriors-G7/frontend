f809ddb03bd03a555db9a0bce43cda18c0737c8f
iuliamariagroza <groza_iuliabv@yahoo.com>
2025-01-16 11:58:12 +0200

    implemented top 10 most ordered products

--- src/routes/AnalyticsDashboard.jsx
+++ src/routes/AnalyticsDashboard.jsx
@@ -1,3 +1,4 @@
+import TopOrderedProducts from "../components/TopOrderedProducts";
 import TopSearchedKeywords from "../components/TopSearchedKeywords";
 
 const AnalyticsDashboard = () => {
@@ -6,7 +7,11 @@ const AnalyticsDashboard = () => {
       <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">
         Analytics Dashboard
       </h1>
-      <TopSearchedKeywords />
+      <div className="grid grid-cols-2 gap-8">
+        <TopSearchedKeywords />
+
+        <TopOrderedProducts />
+      </div>
     </div>
   );
 };
